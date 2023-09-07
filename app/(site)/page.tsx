f2f3a372d4067e8d3d8d389bca6e7d0ddd6f7af9
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-09-06 23:05:24 -0400

    new

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -15,13 +15,6 @@ export default function Home() {
             bg-gray-100">
     
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <Image
-                    alt="Logo"
-                    height="48"
-                    width="48"
-                    className="mx-auto w-auto"
-                    src="/images/logo.png"
-                />
                 <h2 className="
                     mt-6
                     text-center
